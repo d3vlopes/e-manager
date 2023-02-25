097e85ec289ede941fd181b23df2e3b6fcdeb7dd
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-02-25 19:58:58 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,44 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+import { Navbar } from 'components/Navbar'
+
+import { useNavbar } from 'hooks/useNavbar'
+
 export const App = () => {
-  return <h1>Hello World</h1>
+  const { isVisible } = useNavbar()
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route
+            path="/dashboard"
+            element={
+              <div className={!isVisible ? 'page' : 'page page-with-navbar'}>
+                <h1>Dashboard</h1>
+              </div>
+            }
+          />
+          <Route
+            path="/products"
+            element={
+              <div className={!isVisible ? 'page' : 'page page-with-navbar'}>
+                <h1>Products</h1>
+              </div>
+            }
+          />
+          <Route
+            path="/categories"
+            element={
+              <div className={!isVisible ? 'page' : 'page page-with-navbar'}>
+                <h1>Categories</h1>
+              </div>
+            }
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
 }
