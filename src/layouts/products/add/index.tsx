792d04a7af8b23cb28e16d936f8e6fd548c5c3c7
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-02-27 14:10:33 -0300

    feat: create a new product on AddProductPage

--- src/layouts/products/add/index.tsx
+++ src/layouts/products/add/index.tsx
@@ -1,44 +1,111 @@
-import { Button, Input, Select } from 'components'
+import { FormEvent, useState } from 'react'
+import { redirect, useNavigate } from 'react-router-dom'
+import { v4 as uuid } from 'uuid'
 
-import { selectOptionMock } from 'components/Form/Select/mock'
+import { ProductAPI } from 'api'
+
+import { getLocalStorageData, setLocalStorageData } from 'utils'
+
+import { Button, Input } from 'components'
 
 import { BaseLayout } from 'layouts/base'
 
 import * as S from './styles'
 
+type Product = Partial<ProductAPI>
+
 export const AddProductLayout = () => {
+  const [values, setValues] = useState({
+    id: uuid(),
+    name: '',
+    category: '',
+    price: '',
+    quantity: '',
+  } as Product)
+
+  const [error, setError] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  const navigate = useNavigate()
+
+  const handleInput = (field: string, value: string | number) => {
+    setValues((s) => ({ ...s, [field]: value }))
+  }
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    //validação
+    if (!values.name || !values.category || !values.price || !values.quantity)
+      return
+
+    try {
+      setIsLoading(true)
+
+      const products = getLocalStorageData()
+
+      setLocalStorageData([...products, values])
+
+      alert('Produto adicionado com sucesso')
+
+      navigate('/products')
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
     <BaseLayout title="Adicionar produto">
       <S.Wrapper>
-        <S.InputGroup>
-          <Input label="Nome" placeholder="Nome do produto" />
+        <form onSubmit={handleSubmit}>
+          <S.InputGroup>
+            <Input
+              label="Nome"
+              placeholder="Nome do produto"
+              value={values.name}
+              isError={Boolean(error.length > 0)}
+              error={error}
+              onChange={(event) =>
+                handleInput('name', event.currentTarget.value)
+              }
+            />
 
-          <Select label="Nome" options={selectOptionMock} />
+            <Input
+              label="Categoria"
+              placeholder="Categoria do produto"
+              value={values.category}
+              isError={Boolean(error)}
+              error={error}
+              onChange={(event) =>
+                handleInput('category', event.currentTarget.value)
+              }
+            />
 
-          <Input type="number" label="Preço" placeholder="Preço do produto" />
+            <Input
+              type="number"
+              label="Preço"
+              placeholder="Preço do produto"
+              value={values.price}
+              onChange={(event) =>
+                handleInput('price', event.currentTarget.value)
+              }
+            />
 
-          <S.RadioWrapper>
-            <span>Em estoque</span>
+            <Input
+              type="number"
+              label="Quantidade"
+              placeholder="Quantidade em estoque"
+              value={values.quantity}
+              onChange={(event) =>
+                handleInput('quantity', event.currentTarget.value)
+              }
+            />
+          </S.InputGroup>
 
-            <S.RadioContainer>
-              <input type="radio" id="in-stock" name="stock" value="true" />
-              <label htmlFor="in-stock">Sim</label>
-            </S.RadioContainer>
-
-            <S.RadioContainer>
-              <input type="radio" id="no-stock" name="stock" value="false" />
-              <label htmlFor="no-stock">Não</label>
-            </S.RadioContainer>
-          </S.RadioWrapper>
-
-          <Input
-            type="number"
-            label="Quantidade"
-            placeholder="Quantidade em estoque"
-          />
-        </S.InputGroup>
-
-        <Button>Adicionar</Button>
+          <Button disabled={isLoading}>Adicionar</Button>
+        </form>
       </S.Wrapper>
     </BaseLayout>
   )
--- src/layouts/products/add/styles.ts
+++ src/layouts/products/add/styles.ts
@@ -1,32 +1,17 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 2.4rem;
+
+  form > button {
+    margin-top: 2.4rem;
+  }
 `
 
 export const InputGroup = styled.div`
   display: flex;
   flex-direction: column;
   gap: 2.4rem;
-`
-
-export const RadioWrapper = styled.div`
-  ${({ theme }) => css`
-    display: flex;
-    flex-direction: column;
-    flex: 1;
-    accent-color: ${theme.colors.primary['500']};
-
-    span {
-      margin: 0.8rem 0;
-      color: ${theme.colors.neutral[600]};
-    }
-  `}
-`
-
-export const RadioContainer = styled.div`
-  display: flex;
-  gap: 1.6rem;
 `
