ed15e5c566c604da1d5c756ab228a1f212efb0a1
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-03-01 18:03:14 -0300

    chore: add react-hot-toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { ThemeProvider } from 'styled-components'
+import { Toaster } from 'react-hot-toast'
+
+import { theme } from 'styles/theme'
+import { GlobalStyles } from 'styles/global'
+
 import { Routes } from 'routes'
 
 export const App = () => {
-  return <Routes />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Toaster />
+        <Routes />
+        <GlobalStyles />
+      </ThemeProvider>
+    </>
+  )
 }
