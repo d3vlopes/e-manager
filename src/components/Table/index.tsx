31a790ebfc6201664271ecf205a73481af38edd6
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-03-02 00:54:46 -0300

    feat: add useMemo on Table component

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable react/display-name */
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useMemo } from 'react'
+
 import { Product } from 'domain/Product'
 
 import { TableDataTemplate } from './template/TableDataTemplate'
@@ -10,23 +14,27 @@ export type TableProps = {
   data: DataTable[]
 }
 
-export const Table = ({ data }: TableProps) => {
-  return (
-    <S.Wrapper>
-      <S.Table>
-        <S.Thead>
-          <S.Tr>
-            <S.Th>ID</S.Th>
-            <S.Th>Nome</S.Th>
-            <S.Th>Categoia</S.Th>
-            <S.Th>Preço</S.Th>
-            <S.Th>Quantidade</S.Th>
-            <S.Th>Ações</S.Th>
-          </S.Tr>
-        </S.Thead>
+export const Table = useMemo(
+  () =>
+    ({ data }: TableProps) => {
+      return (
+        <S.Wrapper>
+          <S.Table>
+            <S.Thead>
+              <S.Tr>
+                <S.Th>ID</S.Th>
+                <S.Th>Nome</S.Th>
+                <S.Th>Categoia</S.Th>
+                <S.Th>Preço</S.Th>
+                <S.Th>Quantidade</S.Th>
+                <S.Th>Ações</S.Th>
+              </S.Tr>
+            </S.Thead>
 
-        <S.Tbody>{data.map(TableDataTemplate)}</S.Tbody>
-      </S.Table>
-    </S.Wrapper>
-  )
-}
+            <S.Tbody>{data.map(TableDataTemplate)}</S.Tbody>
+          </S.Table>
+        </S.Wrapper>
+      )
+    },
+  [],
+)
