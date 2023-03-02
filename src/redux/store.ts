8577f43e2e5c9759d75b06077ba5ca95fee36d8c
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-03-02 00:25:21 -0300

    feat: add redux store

---
+++ src/redux/store.ts
@@ -0,0 +1,25 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import { productsReducer } from './features/product/productSlice'
+
+export const store = configureStore({
+  reducer: {
+    products: productsReducer,
+  },
+})
+
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION__: any
+  }
+}
+
+if (
+  process.env.NODE_ENV === 'development' &&
+  window.__REDUX_DEVTOOLS_EXTENSION__
+) {
+  window.__REDUX_DEVTOOLS_EXTENSION__.connect()
+}
