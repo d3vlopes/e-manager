1d1a05fdf1294400eb79f051db522148abf39f45
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-03-02 13:38:25 -0300

    fix: useMemo Table component

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -14,27 +14,23 @@ export type TableProps = {
   data: DataTable[]
 }
 
-export const Table = useMemo(
-  () =>
-    ({ data }: TableProps) => {
-      return (
-        <S.Wrapper>
-          <S.Table>
-            <S.Thead>
-              <S.Tr>
-                <S.Th>ID</S.Th>
-                <S.Th>Nome</S.Th>
-                <S.Th>Categoia</S.Th>
-                <S.Th>Preço</S.Th>
-                <S.Th>Quantidade</S.Th>
-                <S.Th>Ações</S.Th>
-              </S.Tr>
-            </S.Thead>
+export const Table = ({ data }: TableProps) => {
+  return (
+    <S.Wrapper>
+      <S.Table>
+        <S.Thead>
+          <S.Tr>
+            <S.Th>ID</S.Th>
+            <S.Th>Nome</S.Th>
+            <S.Th>Categoia</S.Th>
+            <S.Th>Preço</S.Th>
+            <S.Th>Quantidade</S.Th>
+            <S.Th>Ações</S.Th>
+          </S.Tr>
+        </S.Thead>
 
-            <S.Tbody>{data.map(TableDataTemplate)}</S.Tbody>
-          </S.Table>
-        </S.Wrapper>
-      )
-    },
-  [],
-)
+        <S.Tbody>{data.map(TableDataTemplate)}</S.Tbody>
+      </S.Table>
+    </S.Wrapper>
+  )
+}
