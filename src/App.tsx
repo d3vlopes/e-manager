14bde89a1a60e2607423a10bab3b539eb9f2be56
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-03-02 00:26:19 -0300

    feat: add redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,5 @@
-import { ThemeProvider } from 'styled-components'
-import { Toaster } from 'react-hot-toast'
-
-import { theme } from 'styles/theme'
-import { GlobalStyles } from 'styles/global'
-
 import { Routes } from 'routes'
 
 export const App = () => {
-  return (
-    <>
-      <ThemeProvider theme={theme}>
-        <Toaster />
-        <Routes />
-        <GlobalStyles />
-      </ThemeProvider>
-    </>
-  )
+  return <Routes />
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { Provider } from 'react-redux'
+import { ThemeProvider } from 'styled-components'
+import { Toaster } from 'react-hot-toast'
+
+import { theme } from 'styles/theme'
+import { GlobalStyles } from 'styles/global'
+
+import { store } from 'redux/store'
+
 import { App } from 'App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <Toaster />
+
+        <App />
+        <GlobalStyles />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
